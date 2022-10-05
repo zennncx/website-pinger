9dc61949fdfc8cf83a26d08c98f8644d4b6e44e0
zennncx <87260173+zennncx@users.noreply.github.com>
2022-10-05 16:17:47 +0800

    Add files via upload

---
+++ source/server.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+const Links = ["https://www.npmjs.com/package/colors"] // ["", "url", "url"]
+const colors = require("colors") //styles
+
+console.log("Please wait...".green)
+if (Links.length < 1) {
+    console.log("No links found.".red)
+    return process.exit()
+}
+Links.forEach(async (url) => {
+    setInterval(async () => {
+        try {
+            const response = await fetch(url);
+            if (response.status == 200) {
+                console.log(`Link :- ${url}・Status :- ${response.status}\n`.green);
+            } else {
+                console.log(`Link :- ${url}・Status :- ${response.status}\n`.yellow);
+            }
+        } catch (error) {
+            console.log(`Link :- ${url}・Error :- ${error}\n`.red);
+        }
+    }, 10000);
+})
+
+require('http').createServer((req, res) => res.end('Server Started')).listen(2000)
